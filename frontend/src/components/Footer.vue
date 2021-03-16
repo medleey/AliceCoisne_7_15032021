16d36b36f97f4ee194ff6073980c4c4e2b9bf636
Alice <coisne.alice@gmail.com>
2021-03-16 19:37:38 +0100

    setting up the file

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="navbar navbar-expand-sm navbar-dark bg-secondary mb-3 py-0">
+      <div class="container">
+          <img src="@/assets/logo.png" alt="groupomania logo" class="gm-logo" />
+        </div>
+        <p>Mentions légales</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+ 
+};
+</script>
+
+<style>
+    
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div>
+      <nav
+        class="navbar navbar-expand-sm navbar-dark bg-secondary mb-3 py-0"
+      >
+        <div class="container">
+          <img src="@/assets/logo.png" alt="groupomania logo" class="gm-logo" />
+        </div>
+
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a href="/" class="nav-link"> Se connecter </a>
+          </li>
+          <li class="nav-item">
+            <a href="/" class="nav-link"> S'inscrire </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+ 
+};
+</script>
+
+<style>
+    .gm-logo {
+        width: 150px;
+    }
+
+    .gm-banner {
+        background-color: grey;
+    }
+</style>
