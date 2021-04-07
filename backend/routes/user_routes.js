054d8efa85213ba1dd50950763b579452720245b
Alice <coisne.alice@gmail.com>
2021-04-07 18:07:12 +0200

    edit back- user routes

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router(); 
 const userCtrl = require('../controllers/users_controller');
 const auth = require("../middleware/auth");
-const multer = require("../config/multer-config");
+const multer = require("../middleware/multer-config");
 
 router.post('/register', userCtrl.register);
 router.post('/login', userCtrl.login);
 router.put('/:id', auth, multer, userCtrl.update);
 router.get('/:id', auth, userCtrl.getOneUser);
-router.delete("/:id", auth, usersCtrl.deleteUser);
+router.delete("/:id", auth, userCtrl.deleteUser);
 
 
 module.exports= router;
--- frontend/src/components/Comment.vue
+++ frontend/src/components/Comment.vue
@@ -8,7 +8,7 @@
         </b-list-group-item>
         <div class="p-1 ml-2 comment-user">
             <p class="mb-0"><b>{{this.comment.User.firstName}} {{this.comment.User.lastName}}</b>
-            <button class="delete-btn" @click="deleteOneComment"><i class="fas fa-times"></i></button>
+            <button class="delete-btn" ><i class="fas fa-times"></i></button>
             </p>
             
             <p class="mb-0">{{this.comment.content}}</p>
@@ -29,7 +29,7 @@ export default {
       "comment"
   ],
  methods: {
-   deleteComment
+  
  }
 } 
 </script>
